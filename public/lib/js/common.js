1eca26d384bfbd92e876572fa3d886f9e10a0c23
April Coloma <april.coloma@ninthdesign.com>
2013-11-15 15:03:22 +0800

    add search input detection

--- public/lib/js/common.js
+++ public/lib/js/common.js
@@ -11,7 +11,12 @@ var listSearchableItems = function(a) {
 };
 
 
+var searchFilter = document.getElementById('search_box_txt'),
+    searchFilterValue = document.getElementById('search_box_txt').value;
 
+searchFilter.onkeyup = function () {
+   alert("input detected");
+}
 
 
 
