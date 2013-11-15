c8f2fa5b98d6b7fd5ed3232ad35c46d0c35afa8f
April Coloma <april.coloma@ninthdesign.com>
2013-11-15 10:32:14 +0800

    DOM experiment

--- public/lib/js/common.js
+++ public/lib/js/common.js
@@ -1,1 +1,8 @@
-alert("hello world!")
+
+var searchableAreaGroup = document.getElementById('searchable-group');
+var searchableElement = searchableAreaGroup.getElementsByTagName('span');
+var descList = searchableElement.length;
+for (var i = 0; i <= descList; i++) {
+    console.log(searchableElement[i].innerHTML);
+}
+
