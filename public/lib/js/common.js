38b1c2cb7dcc7e023963bf73d49ca8069fb1cc8b
April Coloma <april.coloma@ninthdesign.com>
2013-11-15 14:08:31 +0800

    edit html and create function for searchable items

--- public/lib/js/common.js
+++ public/lib/js/common.js
@@ -1,8 +1,23 @@
 
-var searchableAreaGroup = document.getElementById('searchable-group');
-var searchableElement = searchableAreaGroup.getElementsByTagName('span');
-var descList = searchableElement.length;
-for (var i = 0; i <= descList; i++) {
-    console.log(searchableElement[i].innerHTML);
-}
+
+var listSearchableItems = function(a) {
+    var searchableAreaGroup = document.getElementById(a),
+        searchableElement = searchableAreaGroup.getElementsByClassName('searchable'),
+        descList = searchableElement.length;
+
+    for (var i = 0; i < descList; i++) {
+        console.log(searchableElement[i].innerHTML);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
 
