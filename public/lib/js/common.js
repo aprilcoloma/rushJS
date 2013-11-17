80be2e5d3ed0fb10264de57eb71e8edad3b0ec51
April Coloma <april.coloma@ninthdesign.com>
2013-11-18 07:31:54 +0800

    js code cleaning

--- public/lib/js/common.js
+++ public/lib/js/common.js
@@ -32,97 +32,10 @@
                     }
             }
  
-
-            // for (var i = 0; i < descList; i++) {
-            //     var foundItems = searchableElement[i].innerHTML,
-            //         arrayEachItems = arrayItems.push(foundItems);
-
-            //     if (searchFilterValue === arrayItems[i]) {
-            //         var matchedItem = arrayItems[i];
-            //         console.log(matchedItem);
-            //     } else {
-            //         // console.log("No match found.");
-            //     }
-            // }
         };
 
         listSearchableItems('searchable-group');
     };
-
-
-// var listSearchableItems = function(a) {
-//     var searchableAreaGroup = document.getElementById(a),
-//         searchableElement = searchableAreaGroup.getElementsByClassName('searchable'),
-//         descList = searchableElement.length,
-//         arrayItems = [];
-        
-
-//     for (var i = 0; i < descList; i++) {
-//         var foundItems = searchableElement[i].innerHTML; 
-//         arrayEachItems = arrayItems.push(foundItems);    
-//     }
-    
-//     console.log(arrayItems);
-//     var arrayItemsCount = arrayItems.length;
-
-//     var searchFilter = document.getElementById('search_box_txt'),
-//         searchFilterValue = document.getElementById('search_box_txt').value;
-
-//     // function detectInput() {
-//     //     console.log(searchFilterValue);
-//     //    // if (searchFilterValue > 0 ) {
-//     //    //      var searchPattern = new RegExp(/^[a-zA-Z0-9-]+$/);
-
-//     //    //      if (searchFilterValue.match(searchPattern)) {
-//     //    //          for(x = 0; x < arrayItemsCount; x++) {
-                    
-//     //    //          }
-                
-
-//     //    //      } else {
-//     //    //          alert(false);
-//     //    //      }
-
-
-//     //    // } else {
-//     //    //      return false;
-//     //    // }
-
-//     // };
-
-    
-
-// };
-
-// listSearchableItems('searchable-group');
-
-
-
-
-
-/** SEARCH FILTER **/
-// var searchFilter = document.getElementById('search_box_txt'),
-//     searchFilterValue = document.getElementById('search_box_txt').value;
-
-// searchFilter.onkeyup = function () {
-//    alert("input detected");
-// }
-
-
-
-/** REGEX TEST **/
-// var textArray = ["hello", "Perry", "catching-fire-cover.jpg", "Katy", "jay-z"],
-//  arrLen = textArray.length;
-
-// for(i=0; i < arrLen; i++) {
-//  // console.log(textArray[i]);
-
-//  var textPat = new RegExp(/^[a-zA-Z0-9-]+$/),
-//      repText = textArray[i].match(textPat);
-// };
-
-
-
 
 
 
